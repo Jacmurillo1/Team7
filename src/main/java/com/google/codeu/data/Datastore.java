d37aa169aad6f7d6714f6ee8e37d8b14abc74b76
mayyo <mayyoon.myp@gmail.com>
2019-02-23 20:35:42 -0600

    statsHTML

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -16,13 +16,10 @@
 
 package com.google.codeu.data;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.*;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.SortDirection;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -79,4 +76,13 @@ public class Datastore {
 
     return messages;
   }
+
+    /**
+     * Returns the total number of messages for all users.
+     */
+    public int getTotalMessageCount() {
+        Query query = new Query("Message");
+        PreparedQuery results = datastore.prepare(query);
+        return results.countEntities(FetchOptions.Builder.withLimit(1000));
+    }
 }
